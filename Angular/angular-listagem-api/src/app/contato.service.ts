93ed081c9fda944112277d73e200145fc31fc8c0
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-08-09 22:55:29 -0300

    parâmetro de listagem única

--- Angular/angular-listagem-api/src/app/contato.service.ts
+++ Angular/angular-listagem-api/src/app/contato.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs/internal/Observable';
  
 @Injectable({
   providedIn: 'root'
@@ -7,11 +8,15 @@ import { HttpClient } from '@angular/common/http'
 export class ContatoService {
 
   contatosU = 'http://localhost:8080/cliente'
+  contatosU2 = 'http://localhost:8080/cliente/3';
 
   constructor(private http: HttpClient) { }
 
   listar() {
     return this.http.get<any[]>(this.contatosU); // Use template literal without $ sign
+  }
+  listar2(id: number): Observable<any> {
+    return this.http.get<any>(`${this.contatosU}/${id}`); // Use template literal without $ sign
   }
   
 }
--- Angular/angular-listagem-api/src/app/contatos-listagem/contatos-listagem.component.ts
+++ Angular/angular-listagem-api/src/app/contatos-listagem/contatos-listagem.component.ts
@@ -8,14 +8,17 @@ import { ContatoService } from '../contato.service';
 })
 export class ContatosListagemComponent implements OnInit {
   contatos: Array<any> | undefined;
+  contato: any | undefined; // Change from array to single object
 
   constructor(private contatosService: ContatoService){}
   ngOnInit(): void {
     this.listar();
+    this.listar2(3); // Replace with the desired ID
   }
 
   listar() {
     this.contatosService.listar().subscribe(res => this.contatos = res); // Fix typo "subscrible" and use "res" instead of "dados"
   }
-  
-}
+  listar2(id: number) {
+    this.contatosService.listar2(id).subscribe(res => this.contato = res); // Assign the single object
+  }}
